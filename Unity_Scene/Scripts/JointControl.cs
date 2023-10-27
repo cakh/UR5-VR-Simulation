96ad13ac44d5f97a33eef097643c45ebad27692d
Akhilraj Anilkumar <64953988+cakh@users.noreply.github.com>
2023-10-27 16:44:47 +0200

    Add files via upload

---
+++ Unity_Scene/Scripts/JointControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Robotics.UrdfImporter;
+
+public class JointControl : MonoBehaviour
+{
+    Unity.Robotics.UrdfImporter.Control.Controller controller;
+
+    public Unity.Robotics.UrdfImporter.Control.RotationDirection direction;
+    public Unity.Robotics.UrdfImporter.Control.ControlType controltype;
+    public float speed ;
+    public float angle;
+    public float torque ;
+    public float acceleration;
+    public ArticulationBody joint;
+
+
+    void Start()
+    {
+        direction = 0;
+        angle = 0.0f;
+        controller = (Unity.Robotics.UrdfImporter.Control.Controller)this.GetComponentInParent(typeof(Unity.Robotics.UrdfImporter.Control.Controller));
+        joint = this.GetComponent<ArticulationBody>();
+        controller.UpdateControlType(this);
+        speed = controller.speed;
+        torque = controller.torque;
+        acceleration = controller.acceleration;
+    }
+
+    void FixedUpdate(){
+
+        speed = controller.speed;
+        torque = controller.torque;
+        acceleration = controller.acceleration;
+
+
+        if (joint.jointType != ArticulationJointType.FixedJoint)
+        {
+            if (controltype == Unity.Robotics.UrdfImporter.Control.ControlType.PositionControl)
+            {
+                ArticulationDrive currentDrive = joint.xDrive;
+                float newTargetDelta = (int)direction * Time.fixedDeltaTime * speed;
+
+                if (joint.jointType == ArticulationJointType.RevoluteJoint)
+                {
+                    if (joint.twistLock == ArticulationDofLock.LimitedMotion)
+                    {
+                        if (newTargetDelta + currentDrive.target > currentDrive.upperLimit)
+                        {
+                            currentDrive.target = currentDrive.upperLimit;
+                        }
+                        else if (newTargetDelta + currentDrive.target < currentDrive.lowerLimit)
+                        {
+                            currentDrive.target = currentDrive.lowerLimit;
+                        }
+                        else
+                        {
+                            //currentDrive.target += newTargetDelta;
+                            currentDrive.target = angle;
+                        }
+                    }
+                }
+
+                joint.xDrive = currentDrive;
+
+            }
+        }
+    }
+}
